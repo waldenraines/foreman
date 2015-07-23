5a089eae03fefc992dac908edbebe467cc53d569
Walden Raines <walden@redhat.com>
2015-07-23 15:54:55 -0400

    hsdfh

--- app/assets/javascripts/common_parameters.js
+++ app/assets/javascripts/common_parameters.js
@@ -1,9 +1,9 @@
 var Editor;
 
-$(document).on('ContentLoad', load_editor);
+$(document).on('ContentLoad', load_param_editor);
 
-function load_editor(){
-    var template_text = $(".template_text");
+function load_param_editor(){
+    var template_text = $(".param_text");
 
     if (template_text.length > 0) {
         create_param_editor(template_text)
@@ -14,13 +14,13 @@ function create_param_editor(item) {
     var value = item.val();
 
     console.log("create editor");
-    item.parent().prepend("<div id='editor2'></div>");
+    item.parent().prepend("<div id='param_editor'></div>");
     //item.hide();
 
-    Editor = ace.edit("editor2");
+    Editor = ace.edit("param_editor");
     Editor.setShowPrintMargin(false);
     Editor.renderer.setShowGutter(false);
-    $(document).on('resize','#editor2', function(){Editor.resize()});
+    $(document).on('resize','#param_editor', function(){Editor.resize()});
     if (item.is(':disabled')) {
         $('.ace_text-input').attr('disabled', true)
     }
