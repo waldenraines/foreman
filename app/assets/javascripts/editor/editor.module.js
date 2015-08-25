810da9fa2185227586c15feda87a57a440b8c420
Walden Raines <walden@redhat.com>
2015-08-25 14:44:22 -0400

    hsdfh

--- app/assets/javascripts/editor/editor.module.js
+++ app/assets/javascripts/editor/editor.module.js
@@ -9,6 +9,7 @@ angular.module('foreman.editor', ['foreman-templates', 'ui.ace']);
 
 angular.module('foreman.editor').run(function($templateCache){
   console.log($templateCache);
+  console.log($templateCache.info());
   console.log($templateCache.get('editor.html'));
   console.log($templateCache.get('blah.html'));
 });
